c45dfc8a65c1af7372a0d3602ef523cc1d7e1c7a
minibleyz <a32526132@gmail.com>
2026-08-30 23:57:56 +0300

    Тема Compose (цвета/шрифты ITDO)

---
+++ app/src/main/java/ru/itdo/tv/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package ru.itdo.tv.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object ItdoColors {
+    val BgPrimary = Color(0xFF22190F)
+    val BgSecondary = Color(0xFF2B2015)
+    val BlockHoverBg = Color(0xFF33261A)
+    val ChatWindow = Color(0xFF180F08)
+
+    val TextPrimary = Color(0xFFF0E4D0)
+    val TextSecondary = Color(0xFFAB9578)
+
+    val AccentPrimary = Color(0xFFD68A4C)
+    val AccentSecondary = Color(0xFFB0602F)
+    val AccentHover = Color(0xFFE3A066)
+    val AccentLike = Color(0xFFE0685A)
+
+    val LiveRed = Color(0xFFE0685A)
+
+    val AccentGradient = listOf(Color(0xFFC9A26A), Color(0xFF8B5E34))
+}
